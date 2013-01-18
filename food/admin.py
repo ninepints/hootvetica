57b70ae92bd0572a70f8b526d4d5ab2b9ca97f8e
ninepints <dev@ninepints.co>
2013-01-18 16:03:55 -0600

    Minor admin update

--- food/admin.py
+++ food/admin.py
@@ -4,6 +4,5 @@ from food.models import Location
 class LocationAdmin(admin.ModelAdmin):
     fields = ('name', 'uid', 'open')
     list_display = ('name', 'uid', 'open')
-    list_editable = ('open',)
 
 admin.site.register(Location, LocationAdmin)
