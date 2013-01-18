1edf9e65466ca168470718af0524169bc663fe49
ninepints <dev@ninepints.co>
2013-01-18 16:02:54 -0600

    Revert "Removed most editing views and updated URLs"
    This reverts commit 1cae75c42af6fc71b415e4c97f46d40d0287c37a.

--- food/urls.py
+++ food/urls.py
@@ -1,12 +1,25 @@
 from django.conf.urls import patterns, url
-from django.views.generic import ListView
-from food.views import LocationView, ItemUpdateView
+from food.views import (LocationView, LocationUpdateView, CategoryCreateView,
+                        CategoryUpdateView,CategoryDeleteView, ItemCreateView,
+                        ItemUpdateView, ItemDeleteView)
 
 urlpatterns = patterns('',
-    (r'^$', ListView.as_view(queryset=Location.objects.all(),
-        template_name='overview.html')),
     url(r'^location/(?P<pk>[\w-]+)/$', LocationView.as_view(),
         name='location'),
+    url(r'^location/(?P<pk>[\w-]+)/edit/$', LocationUpdateView.as_view(),
+        name='location-edit'),
+
+    url(r'^category/add/$', CategoryCreateView.as_view(),
+        name='category-add'),
+    url(r'^category/(?P<pk>\w+)/edit/$', CategoryUpdateView.as_view(),
+        name='category-edit'),
+    url(r'^category/(?P<pk>\w+)/delete/$', CategoryDeleteView.as_view(),
+        name='category-del'),
+
+    url(r'^item/add/$', ItemCreateView.as_view(),
+        name='item-add'),
     url(r'^item/(?P<pk>\w+)/edit/$', ItemUpdateView.as_view(),
         name='item-edit'),
+    url(r'^item/(?P<pk>\w+)/delete/$', ItemDeleteView.as_view(),
+        name='item-del'),
 )
