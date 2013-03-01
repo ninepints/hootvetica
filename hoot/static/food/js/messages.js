460eae491c099974a9e68e537a8e9677b9877740
John Draper <git@ninepints.co>
2013-03-01 14:05:35 -0600

    Fixed messsage styling for long messages

--- hoot/static/food/js/messages.js
+++ hoot/static/food/js/messages.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     var messagelist = $('#messagelist')
     var messages = messagelist.children('li');
     var button = $('<a class="close noline">X</a>').on('click', function() {
-        var parent = $(this).parent().addClass('collapsed');
+        var parent = $(this).parent().addClass('hidden');
         setTimeout(function() {
             parent.remove();
             if (messagelist.children('li').length == 0)
