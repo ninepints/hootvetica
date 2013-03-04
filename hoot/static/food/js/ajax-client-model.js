69bbf98bfda4547235ff744706225a38e0e8e7cf
ninepints <dev@ninepints.co>
2013-03-04 03:05:30 -0600

    Moved JS utility methods to separate file

--- hoot/static/food/js/ajax-client-model.js
+++ hoot/static/food/js/ajax-client-model.js
@@ -398,56 +398,17 @@ ajaxClient.model = {};
         });
     };
 
-    // Gets the value of the specified cookie
-    function getCookie(name) {
-        var cookieValue = null;
-        if (document.cookie && document.cookie != '') {
-            var cookies = document.cookie.split(';');
-            for (var i = 0; i < cookies.length; i++) {
-                var cookie = jQuery.trim(cookies[i]);
-                // Does this cookie string begin with the name we want?
-                if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                    break;
-                }
-            }
-        }
-        return cookieValue;
-    }
-
-    // Checks whether the given URL is same-origin
-    // (relative to the current location)
-    function sameOrigin(url) {
-        // URL could be relative or scheme relative or absolute
-        var host = document.location.host; // host + port
-        var protocol = document.location.protocol;
-        var sr_origin = '//' + host;
-        var origin = protocol + sr_origin;
-        // Allow absolute or scheme relative URLs to same origin
-        return url == origin ||
-            url.slice(0, origin.length + 1) == (origin + '/') ||
-            url == sr_origin ||
-            url.slice(0, sr_origin.length + 1) == (sr_origin + '/') ||
-            // or any other URL that isn't scheme relative or absolute
-            // i.e relative.
-            !(/^(\/\/|http:|https:).*/.test(url));
-    }
-
-    // Checks whether the given method is non-destructive
-    function safeMethod(method) {
-        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-    }
-
 
     // Main model initialization
     this.init = function(locationData, rInterval, rIntervalErr, adapter) {
         initLocationData = locationData;
         refreshInterval = rInterval ||  300;
         refreshIntervalErr = rIntervalErr || 30;
-        csrftoken = getCookie('csrftoken');
+        csrftoken = ajaxClient.utils.getCookie('csrftoken');
         jQuery.ajaxSetup({
             beforeSend: function(xhr, settings) {
-                if (!safeMethod(settings.type) && sameOrigin(settings.url))
+                if (!ajaxClient.utils.safeMethod(settings.type) &&
+                    ajaxClient.utils.sameOrigin(settings.url))
                     xhr.setRequestHeader('X-CSRFToken', csrftoken);
             }
         });
