4438d121bcc735fdad518710047b50965544e3a8
ninepints <dev@ninepints.co>
2013-03-31 03:18:18 -0500

    Added Sunday menu items

--- food/management/commands/closeshop.py
+++ food/management/commands/closeshop.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
+from django.db.models import Q
 from django.utils import timezone
-from food.models import Location
+from food.models import Location, Item
 
 class Command(BaseCommand):
     args = '[location ...]'
@@ -16,5 +17,12 @@ class Command(BaseCommand):
             count = Location.objects.all().update(
                 open=False,
                 last_modified=timezone.now())
+        self.do_hardcoded_menu_updates()
         self.stdout.write(
             'Closed {} location{}'.format(count, '' if count == 1 else 's'))
+
+    def do_hardcoded_menu_updates(self):
+        # This could be nicer (i.e. not hardcoded) but I don't think
+        # doing it properly is worth the time commitment
+
+        Item.objects.filter(Q(name='HBCB') | Q(name='Specialty')).delete()
