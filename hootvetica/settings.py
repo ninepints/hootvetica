c4ebebe7ad6e19b83958a3b5f75d6acf3dce3ad8
John Draper <git@ninepints.co>
2013-08-03 14:53:54 -0400

    Cleaned up unused settings and added readme

--- hootvetica/settings.py
+++ hootvetica/settings.py
@@ -56,15 +56,6 @@ AWS_QUERYSTRING_AUTH = False
 GOOGLE_ANALYTICS_ID = os.environ.get('GOOGLE_ANALYTICS_ID')
 GOOGLE_ANALYTICS_DOMAIN = os.environ.get('GOOGLE_ANALYTICS_DOMAIN')
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
-
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
